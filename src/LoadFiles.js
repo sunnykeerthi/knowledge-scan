c592dcd8f23ef68f58c314537821d7302f7e9f56
rkeerthi <sunnykeerthi@gmail.com>
2022-04-27 15:20:42 +0530

    Updated to accept and process multi files

--- src/LoadFiles.js
+++ src/LoadFiles.js
@@ -4,8 +4,12 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+
 const fields = [
   "Link ID",
+  "Publisher ID",
+  "Listing ID",
+  "Location ID",
   "Publisher",
   "Location Name",
   "Location Address",
@@ -16,33 +20,39 @@ const fields = [
   "Name match",
   "Address match",
   "Phone match",
+  "Match Status",
 ];
+
 const Load = () => {
   const [files, setFiles] = useState([]);
   const [allData, setAllData] = useState([]);
   const [isDone, setIsDone] = useState(false);
   const [processingData, setProcessingData] = useState(false);
   const [value, setValue] = useState("1");
+  const [csvArray, setCsvArray] = useState([]);
+  const [currIndex, setCurrIndex] = useState(0);
+  const [fileNum, setFileNum] = useState();
   const handleTabChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const processCSV = (str, delim = ",") => {
+  const processCSV = (str, fileName, delim = ",") => {
     const headers = str.slice(0, str.indexOf("\n")).split(delim);
     const rows = str.slice(str.indexOf("\n") + 1, str.length - 1).split("\n");
     let nArray = rows.map((row) => {
       const values = row.split(delim);
       const eachObj = headers.reduce((obj, header, i) => {
-        if (fields.includes(header)) obj[header] = values[i];
+        obj[header] = values[i];
         return obj;
       }, {});
       return eachObj;
     });
-    createKeys(nArray);
+    createKeys(nArray, fileName);
   };
 
-  const createKeys = async (data) => {
+  const createKeys = async (data, fileName) => {
     const dataToSet = {
+      fileName: fileName,
       keys: Object.keys(data.length ? data[0] : {}),
       values: data.filter((o) => Object.keys(o).length),
     };
@@ -58,13 +68,21 @@ const Load = () => {
     }
   }, [allData, isDone, files, processingData]);
 
+  useEffect(() => {
+    if (allData.length >= 1) {
+      let currArr = allData;
+      currArr[fileNum].values = csvArray;
+      setAllData(currArr);
+    }
+  }, [csvArray]);
+
   const handleUpload = (e) => {
     Array.from(files).forEach((file) => {
       const currFile = file;
       const reader = new FileReader();
       reader.onload = (e) => {
         const text = e.target.result;
-        processCSV(text);
+        processCSV(text, file.name.split(".").slice(0, -1).join("."));
       };
       reader.readAsText(currFile);
     });
@@ -74,13 +92,12 @@ const Load = () => {
     e.preventDefault();
     setFiles(e.target.files);
   };
-
   const processData = (e) => {
     var currData = allData;
     setAllData([]);
     currData.forEach((item) => {
       let rowWithSum = item.values.map((item1) => {
-        let nameMatch, addressMatch, phoneMatch;
+        let nameMatch, addressMatch, phoneMatch, botSuggest;
         if (
           replaceSplChars(
             item1["Location Address"] +
@@ -104,14 +121,21 @@ const Load = () => {
         if (item1["Location Name"] === item1["Listing Name"])
           nameMatch = "True";
         else nameMatch = "False";
+        let arr = [nameMatch, addressMatch, phoneMatch];
+        botSuggest =
+          arr.filter((x) => x.toLowerCase() === "true").length >= 2
+            ? "yes"
+            : "no";
+
         return {
           ...item1,
           "Name match": nameMatch,
           "Address match": addressMatch,
           "Phone match": phoneMatch,
+          "Match Status": botSuggest,
         };
       });
-      createKeys(rowWithSum);
+      createKeys(rowWithSum, item.fileName);
       setProcessingData(true);
     });
   };
@@ -124,11 +148,39 @@ const Load = () => {
     const fWrapper = document.getElementsByTagName("body")[0];
     fWrapper.classList.toggle("dark");
   };
+  const csvLink = () => {
+    allData.forEach((parent) => {
+      let nMap = [];
+      parent.values.forEach((item) => {
+        let obj = {
+          locationId: item["Location ID"],
+          partnerId: item["Publisher ID"],
+          listingId: item["Location ID"],
+          override: item["Match Status"],
+        };
+        nMap.push(obj);
+      });
+      getCSV(nMap, parent.fileName);
+    });
+  };
+
+  const getCSV = (object, fileName) => {
+    let csv = Object.entries(Object.entries(object)[0][1])
+      .map((e) => e[0])
+      .join(",");
+    for (const [k, v] of Object.entries(object)) {
+      csv += "\r\n" + Object.values(v).join(",");
+    }
+    let j = document.createElement("a");
+    j.download = fileName + ".csv";
+    j.href = URL.createObjectURL(new Blob([csv]));
+    j.click();
+  };
 
   return (
     <div id="formWrapper">
       <div id="centerContent">
-        <div class="toggleContainer">
+        <div className="toggleContainer">
           <label className="switch">
             <input type="checkbox" onChange={(e) => changeColor(e)} />
             <span className="slider round"></span>
@@ -161,21 +213,32 @@ const Load = () => {
         >
           Processdata
         </button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            csvLink();
+          }}
+        >
+          Export to CSV
+        </button>
       </div>
       {isDone && allData.length >= 1 && (
         <TabContext value={value}>
           <TabList onChange={handleTabChange}>
             {files.length >= 1 &&
-              [...files].map((item1, idx) => (
-                <Tab label={`Label ${idx + 1}`} value={`${idx + 1}`} />
+              [...allData].map((item1, idx) => (
+                <Tab label={item1.fileName} value={`${idx + 1}`} />
               ))}
           </TabList>
           {files.length >= 1 &&
             [...allData].map((item1, idx) => (
               <TabPanel value={`${idx + 1}`}>
                 <RenderTable
-                  keys={item1.keys}
                   csvArray={item1.values}
+                  setCsvArray={setCsvArray}
+                  fileNum={idx}
+                  setFileNum={setFileNum}
+                  setCurrIndex={setCurrIndex}
                 ></RenderTable>
               </TabPanel>
             ))}
--- src/RenderTable.js
+++ src/RenderTable.js
@@ -5,27 +5,110 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+const fields = [
+  "Link ID",
+  "Publisher ID",
+  "Listing ID",
+  "Location ID",
+  "Publisher",
+  "Location Name",
+  "Location Address",
+  "Location Phone",
+  "Listing Name",
+  "Listing Address",
+  "Listing Phone",
+  "Name match",
+  "Address match",
+  "Phone match",
+  "Match Status",
+];
+const RenderTable = ({
+  csvArray,
+  setCsvArray,
+  setCurrIndex,
+  fileNum,
+  setFileNum,
+}) => {
+  const [csvArrayState, setCsvArrayState] = useState(csvArray);
+  const handleBlur = (e) => {
+    e.currentTarget.classList.add("noBorder");
+    e.currentTarget.classList.remove("border");
+    setCsvArray([...csvArrayState]);
+    setFileNum(fileNum);
+  };
 
-const RenderTable = ({ csvArray, keys }) => {
+  useEffect(() => {
+    setCsvArrayState(csvArray);
+  }, [csvArray]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    e.currentTarget.readOnly = false;
+    e.currentTarget.classList.remove("noBorder");
+    e.currentTarget.classList.add("border");
+  };
+
+  function getHeadings(data) {
+    return Object.keys(data[0]).map((key) => {
+      if (fields.includes(key)) return <TableCell>{key}</TableCell>;
+    });
+  }
+
+  // `map` over the data to return
+  // row data, passing in each mapped object
+  // to `getCells`
+  function getRows(data) {
+    return data.map((obj, idx) => {
+      return (
+        <TableRow>
+          {Object.keys(obj).map((key) => {
+            if (fields.includes(key)) {
+              return getCells(obj[key], idx, key);
+            }
+          })}
+        </TableRow>
+      );
+    });
+  }
+
+  // Return an array of cell data using the
+  // values of each object
+  function getCells(currCell, i, key) {
+    return key === "Match Status" ? (
+      <TableCell>
+        <input
+          className="noBorder"
+          type="true"
+          value={csvArrayState[i]["Match Status"]}
+          onChange={(e) => {
+            const updatedRankFields = csvArrayState.map((r, rIdx) => {
+              if (rIdx === i) {
+                return { ...r, "Match Status": e.target.value };
+              } else {
+                return r;
+              }
+            });
+            setCurrIndex(i)
+            setCsvArrayState(updatedRankFields);
+          }}
+          onBlur={(e) => handleBlur(e)}
+          onClick={(e) => handleChange(e)}
+        ></input>
+      </TableCell>
+    ) : (
+      <TableCell>{currCell}</TableCell>
+    );
+  }
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            {keys.map((item, idx) => (
-              <TableCell key={idx}>{item}</TableCell>
-            ))}
-          </TableRow>
+          <TableRow>{getHeadings(csvArrayState)}</TableRow>
         </TableHead>
-        <TableBody>
-          {csvArray.map((item, idx) => (
-            <TableRow>
-              {keys.map((key, idx) => (
-                <TableCell> {item[key]} </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
+
+        <TableBody>{getRows(csvArrayState)}</TableBody>
       </Table>
     </TableContainer>
   );
